049eb2b8af809f2b58b73045e166878de454fa20
simera <olman4jesus@gmail.com>
2024-09-03 11:49:12 +0300

    update

--- controller.js
+++ controller.js
@@ -8,15 +8,15 @@ const controlAdvice = async function () {
 
 	await fetchAdvice(url, (data, id) => {
 		generator(data)
-		updateURLWithAdviceId(id)
+		// updateURLWithAdviceId(id)
 	})
 }
 
-function updateURLWithAdviceId(id) {
-	const url = new URL(window.location)
-	url.searchParams.set("advice", id)
-	window.history.pushState({ path: url.href }, "", url.href)
-}
+// function updateURLWithAdviceId(id) {
+// 	const url = new URL(window.location)
+// 	url.searchParams.set("advice", id)
+// 	window.history.pushState({ path: url.href }, "", url.href)
+// }
 
 const init = function () {
 	console.log("init")
